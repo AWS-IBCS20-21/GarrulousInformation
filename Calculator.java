66bbac91a47fd994e47cc534a2d477d4c9dae6b5
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2020-10-01 13:40:19 -0700

    Most recent update to calculator project

---
+++ Calculator.java
@@ -0,0 +1,61 @@
+/*Lu Henry-Mitchell
+9-29-20
+6th period */
+
+import java.util.*;
+
+public class Calculator
+{
+
+public Calculator()
+{
+
+}
+
+  public static void main (String[] args)
+  {
+    Calculator test = new Calculator();
+    Scanner scanny = new Scanner(System.in);
+    int operation;
+    double num1;
+    double num2;
+    double result;
+
+    System.out.println("This calculator can perform several operations. " +
+    "Enter a number to choose an operation:");
+    System.out.println("1.) Add \n2.) Muliply \n3.) Exponent");
+    operation = scanny.nextInt();
+    System.out.println("Enter a number:");
+    num1 = scanny.nextDouble();
+    System.out.println("Enter a second number:");
+    num2 = scanny.nextDouble();
+
+    if(operation == 1)
+    {
+      System.out.println("The sum of " + num1 + " and " + num2 + " is " + test.add(num1, num2));
+    } else if(operation == 2)
+    {
+      System.out.println("The product of " + num1 + " and " + num2 + " is " + test.multiply(num1, num2));
+    } else if(operation == 3)
+    {
+      System.out.println(num1 + " to the power of " + num2 + " is " + test.toThePowerOf(num1, num2));
+    } else {
+      System.out.println("You don't seem to have entered one of the available options");
+    }
+  }
+
+  public double add(double num1, double num2)
+  {
+    return num1+num2;
+  }
+
+  public double multiply(double num1, double num2)
+  {
+    return num1*num2;
+  }
+
+  public double toThePowerOf(double num1, double num2)
+  {
+    return Math.pow(num1, num2);
+  }
+}
