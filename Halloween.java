27400963fd34a6a53cbcf113b28d2964772532df
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2020-10-27 13:24:58 -0700

    final version of program

--- Halloween.java
+++ Halloween.java
@@ -9,9 +9,9 @@ public class Halloween
   public static void main(String[] args)
   {
 
-    double[][] neighborhood = new double[5][5];
+    double[][] neighborhood = new double[5][5]; //creates 5x5 matrix to represent houses of neighborhood
 
-    int bestCol = 0;
+    int bestCol = 0; //sets up variables to keep track of the highest generosity rating
     int bestRow = 0;
     double bestVal = 0;
 
@@ -31,21 +31,21 @@ public class Halloween
     int row5 = 0;
     double val5 = 0;
 
-    for(int i = 0; i < 5; i++)
-    {
+    for(int i = 0; i < 5; i++) //loops through matrix and
+    {                          //gives each house a random generosity rating
       for(int j = 0; j < 5; j++)
       {
         neighborhood[i][j] = (Math.random())*10;
       }
     }
 
-    for(int k = 0; k < 5; k++)
+    for(int k = 0; k < 5; k++) //goes through each spot of matrix
     {
-      for(int l = 0; l < 5; l++)
+      for(int l = 0; l < 5; l++) //and checks if generosity rating is higher than current max
       {
-        if(neighborhood[k][l] > bestVal)
+        if(neighborhood[k][l] > bestVal) //if not, checks if higher than current second-highest rating, etc
         {
-          bestVal = neighborhood[k][l];
+          bestVal = neighborhood[k][l]; //to establish top 5 highest ratings
           bestRow = k;
           bestCol = l;
         } else if(neighborhood[k][l] > val2)
@@ -72,16 +72,50 @@ public class Halloween
 
         }
       }
-      System.out.println("Top val: " + bestVal);
-      System.out.println("Second val: " + val2);
-      System.out.println("Third val: " + val3);
-      System.out.println("Fourth val: " + val4);
-      System.out.println("Fifth val: " + val5);
 
       //rows = streets
       //columns = xth house on ...
-      //maybe use if statements?
 
-      System.out.println("The first house to visit is house " + bestCol + "on Street " + bestRow);
+      bestCol++;
+      bestRow++;
+      col2++;
+      row2++;
+      col3++;
+      row3++;
+      col4++;
+      row4++;
+      col5++;
+      row5++; //so house and street numbers aren't 1 off (otherwise would start at 0 - ex "House 0")
+
+      //add street names... somehow
+      String[] streetNames = new String[5]; //creates empty array of street names to correspond to numbers of rows
+      int[] rows = {bestRow, row2, row3, row4, row5}; //puts rows (representing streets) in array so I can go through them more easily
+      for(int m = 0; m < 5; m++)
+      {
+        if(rows[m] == 1) //assigns street name to each street based on number of row
+        {
+          streetNames[m] = "Oak";
+        } else if(rows[m] == 2)
+        {
+          streetNames[m] = "Pine";
+        } else if(rows[m] == 3)
+        {
+          streetNames[m] = "Maple";
+        } else if(rows[m] == 4)
+        {
+          streetNames[m] = "Cedar";
+        } else
+        {
+          streetNames[m] = "Hill";
+        }
+      }
+
+      System.out.println("Welcome to the Halloween Strategizer 2020 Edition"); //prints welcome message
+      System.out.println("Your first destination is house " + bestCol + " on " + streetNames[0] + " Street with a " + Math.round(bestVal*10) + "% chance of candy");
+      System.out.println("Your second destination is house " + col2 + " on " + streetNames[1] + " Street with a " + Math.round(val2*10) + "% chance of candy");
+      System.out.println("Your third destination is house " + col3 + " on " + streetNames[2] + " Street with a " + Math.round(val3*10) + "% chance of candy");
+      System.out.println("Your fourth destination is house " + col4 + " on " + streetNames[3] + " Street with a " + Math.round(val4*10) + "% chance of candy");
+      System.out.println("Your fifth destination is house " + col5 + " on " + streetNames[4] + " Street with a " + Math.round(val5*10) + "% chance of candy");
+      //prints list of ranked destinations
     }
   }
