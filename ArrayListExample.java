b672dfdac1a5543ee7ddeca492982e6b785653db
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2020-11-02 10:33:51 -0800

    with an actual collection this time

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -5,15 +5,15 @@ public class ArrayListExample
 {
   public static void main (String[] args)
   {
-    ArrayList<String> test = new ArrayList<String>();
-    test.add("hello");
-    test.add("does");
-    test.add("this");
-    test.add("work");
+    ArrayList<String> movieTickets = new ArrayList<String>(); //I collect movie tickets
+    movieTickets.add("Bladerunner");
+    movieTickets.add("Avengers: Endgame");
+    movieTickets.add("47 Meters Down: Uncaged");
+    movieTickets.add("A Quiet Place");
 
-    for(int i=0; i<test.size(); i++)
+    for(int i=0; i<movieTickets.size(); i++)
     {
-      System.out.println(test.get(i));
+      System.out.println(movieTickets.get(i));
     }
   }
 }
