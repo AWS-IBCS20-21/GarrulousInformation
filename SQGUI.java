5d4d6c3bfaaa32482c8cb14786b397c53d9b31e9
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2021-02-24 14:50:46 -0800

    skeleton class for GUI part of IA

---
+++ SQGUI.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class SQGUI
+{
+  public JPanel controlPanel;
+  public JFrame mainFrame;
+  public JTextField myTextField;
+  public JButton enterButton;
+
+  public SQGUI()
+  {
+
+  }
+
+  public static void main(String[] args)
+  {
+
+  }
+
+  public int getNumQs()
+  {
+    int numQs = 0;
+    return numQs;
+  }
+
+  public void runQuiz()
+  {
+
+  }
+
+}
