507a017986bbcd48aa270965f263783740d31ef1
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2021-02-27 18:11:31 -0800

    w isSubjunctive working better but not perfectly

--- SubjunctiveQuiz.java
+++ SubjunctiveQuiz.java
@@ -84,16 +84,25 @@ public class SubjunctiveQuiz
     System.out.println(test.isSubjuntive("Ella quiere que bebas agua"));
     System.out.println(test.isIndicative("Ella quiere que bebas agua"));
 
-    System.out.println(test.isSubjuntive("Ella quiere que abras la puerta"));
-    System.out.println(test.isIndicative("Ella quiere que abras la puerta"));
+    System.out.println(test.isSubjuntive("Ella quiere que hables"));
+    System.out.println(test.isIndicative("Ella quiere que hables"));
 
     System.out.println(test.isIndicative("Ella habla conmigo"));
     System.out.println(test.isSubjuntive("Ella habla conmigo"));
+
+
+    //test.runQuiz();
   }
 
   public void runQuiz()
   {
-
+    for(int i = 0; i < sentences.length; i++)
+    {
+      if(isSubjuntive(sentences[i]))
+      {
+        System.out.println("Subjunctive: " + sentences[i]);
+      }
+    }
   }
 
   public String[] generateQs(int numQs)
@@ -106,15 +115,27 @@ public class SubjunctiveQuiz
   {
     boolean stem = false;
     boolean ending = false;
+    boolean hasQue = false;
+    boolean subVerb = false;
+    String temp = "";
+
     String[] words = sentence.split(" ");
 
     for(int i = 0; i < words.length; i++)
     {
+      //95% of subjunctive sentences have 'que,' so check for that to screen out false positives
+      if(words[i].equals("que"))
+      {
+        hasQue = true;
+      }
+
       for(int j = 0; j < ARsubEndings.length; j++)
       {
-        if(words[i].length() > 3 && words[i].substring(words[i].length() - ARsubEndings[j].length()).equals(ARsubEndings[j]))
+        if(words[i].length() > 3 && words[i].endsWith(ARsubEndings[j]))
         {
           ending = true;
+          temp = words[i].substring(0, words[i].length()-ARsubEndings[j].length());
+          //System.out.println(temp); //for debugging
         }
       }
       for(int k = 0; k < ARverbStems.length; k++)
@@ -122,21 +143,25 @@ public class SubjunctiveQuiz
         if(words[i].contains(ARverbStems[k]))
         {
           stem = true;
+          temp = temp.replaceFirst(ARverbStems[k], "");
+          //System.out.println("What's left: " + temp); //for debugging
         }
       }
-      if(ending && stem)
+      if(ending && stem && temp.length() == 0)
       {
-        return true;
+        subVerb = true;
       } else{
         ending = false;
         stem = false;
+        temp = "";
       }
 
       for(int l = 0; l < IRsubEndings.length; l++)
       {
-        if(words[i].length() > 3 && words[i].substring(words[i].length() - IRsubEndings[l].length()).equals(IRsubEndings[l]))
+        if(words[i].length() > 3 && words[i].endsWith(IRsubEndings[l]))
         {
           ending = true;
+          temp = words[i].substring(0, words[i].length()-IRsubEndings[l].length());
         }
       }
       for(int m = 0; m < IRverbStems.length; m++)
@@ -144,29 +169,37 @@ public class SubjunctiveQuiz
         if(words[i].contains(IRverbStems[m]))
         {
           stem = true;
+          temp = temp.replaceFirst(IRverbStems[m], "");
         }
       }
-      if(stem && ending)
+      if(stem && ending && temp.length() == 0)
       {
-        return true;
+        subVerb = true;
       }
       stem = false;
       ending = false;
+      temp = "";
     }
-    return false;
+    if(subVerb && hasQue)
+    {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  public boolean isIndicative(String sentence)
+  public boolean isIndicative(String sentence) //might want to chop off the parts it identifies and then check that nothing is left
   {
     boolean stem = false;
     boolean ending = false;
+
     String[] words = sentence.split(" ");
 
     for(int i = 0; i < words.length; i++)
     {
       for(int j = 0; j < ARindicEndings.length; j++)
       {
-        if(words[i].length() > 3 && words[i].substring(words[i].length() - ARindicEndings[j].length()).equals(ARindicEndings[j]))
+        if(words[i].length() > 3 && words[i].endsWith(ARindicEndings[j]))
         {
           ending = true;
         }
@@ -188,7 +221,7 @@ public class SubjunctiveQuiz
 
       for(int l = 0; l < IRindicEndings.length; l++)
       {
-        if(words[i].length() > 3 && words[i].substring(words[i].length() - IRindicEndings[l].length()).equals(IRindicEndings[l]))
+        if(words[i].length() > 3 && words[i].endsWith(IRindicEndings[l]))
         {
           ending = true;
         }
@@ -207,6 +240,6 @@ public class SubjunctiveQuiz
       stem = false;
       ending = false;
     }
-    return false;
+      return false;
   }
 }
