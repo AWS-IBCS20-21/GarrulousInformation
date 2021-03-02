6df9a788c37b8998818c82bc4014fbe68b04238f
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2021-03-01 16:50:15 -0800

    w start of method for getting input on number of questions

--- SQGUI.java
+++ SQGUI.java
@@ -9,19 +9,45 @@ public class SQGUI
   public JFrame mainFrame;
   public JTextField myTextField;
   public JButton enterButton;
+  public JLabel headerLabel;
+  public JLabel explanationLabel;
 
   public SQGUI()
   {
+    mainFrame = new JFrame("Subjunctive Quiz");
+    mainFrame.setSize(600,300);
+    mainFrame.setLayout(new GridLayout(4, 1));
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    headerLabel = new JLabel("", JLabel.CENTER);
+    explanationLabel = new JLabel("", JLabel.CENTER);
+
+    controlPanel = new JPanel();
+    controlPanel.setLayout(new FlowLayout());
+
+    mainFrame.add(headerLabel);
+    mainFrame.add(explanationLabel);
+    mainFrame.add(controlPanel);
+    mainFrame.setVisible(true);
   }
 
   public static void main(String[] args)
   {
-
+    SQGUI test = new SQGUI();
   }
 
   public int getNumQs()
   {
+    enterButton = new JButton("Enter");
+    //enterButton.addActionListener(new ButtonListener());
+    controlPanel.add(enterButton); //why am I adding it here and not to mainFrame?
+
+    myTextField = new JTextField(10);
+    controlPanel.add(myTextField);
+
+    headerLabel.setText("Welcome to the Subjunctive Quiz");
+    explanationLabel.setText("Enter the number of questions for this session");
+
     int numQs = 0;
     return numQs;
   }
@@ -32,3 +58,12 @@ public class SQGUI
   }
 
 }
+
+//SUPER not working, we'll get back to this
+/*private class ButtonListener implements ActionListener
+{
+  public void actionPerformed(ActionEvent e)
+  {
+    //do something
+  }
+}*/
